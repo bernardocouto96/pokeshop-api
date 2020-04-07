9bcbbec275ff4365f651b7acc71f0bc6b29cfe1c
Bernardo Couto <bernardocouto96@gmail.com>
2020-04-07 15:37:09 -0300

    aula 2
    Co-authored-by: evelyn.correa@b2wdigital.com <evelyn.correa@b2wdigital.com>
    Co-authored-by: alexia.pinto@b2wdigital.com <alexia.pinto@b2wdigital.com>
    Co-authored-by: stephaniknupp@hotmail.com <stephaniknupp@hotmail.com>
    Co-authored-by: Michele-siqueira <michele.siqueira@b2wdigital.com>
    Co-authored-by: camila.barroso@b2wdigital.com <camila.barroso@b2wdigital.com>
    Co-authored-by: andre.barata@b2wdigital.com <andre.barata@b2wdigital.com>
    Co-authored-by: Igor Felipe <igor_play_22@hotmail.com>
    Co-authored-by: laioncorcino@outlook.com <laioncorcino@outlook.com>
    Co-authored-by: laura.rosa@b2wdigital.com <laura.rosa@b2wdigital.com>

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,93 @@
 import express from "express";
+import axios from "axios";
+import mongoose from "mongoose";
+import { Schema, model } from "mongoose";
+
 const app = express();
+app.use(express.json());
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Olá estágio bit!");
+const pokemonSchema = new Schema({
+  number: { type: Number, required: true, unique: true },
+  name: { type: String, required: true },
+  image: { type: String, default: "" },
+  price: { type: Number, default: 0 },
+  type: { type: String, default: "" }
+});
+
+const pokemonModel = model("Pokemon", pokemonSchema);
+
+type PokemonTypes =
+  | "fire"
+  | "water"
+  | "grass"
+  | "fairy"
+  | "dragon"
+  | "bug"
+  | "ghost"
+  | "steel"
+  | "flying"
+  | "poison"
+  | "ground"
+  | "rock"
+  | "electric"
+  | "psychic"
+  | "ice"
+  | "dark"
+  | "normal";
+
+type Pokemon = {
+  number: number;
+  name: string;
+  image: string;
+  price: number;
+  type: PokemonTypes;
+};
+
+app.post("/pokemon", async (req, res) => {
+  const pokemon: Pokemon = req.body;
+
+  try {
+    const dbResponse = await pokemonModel.create(pokemon);
+    res.send(dbResponse);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.get("/pokemon", async (req, res) => {
+  const { query } = req;
+  const dbResponse = await pokemonModel.find(query);
+
+  res.send(dbResponse);
+});
+
+app.get("/pokemon/:name", async (req, res) => {
+  const { name } = req.params;
+
+  const dbResponse = await pokemonModel.find({ name });
+
+  res.send(dbResponse);
+});
+
+mongoose.connect(
+  "mongodb://admin:admin123@ds139067.mlab.com:39067/pokeshop-db",
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true
+  }
+);
+
+mongoose.connection.once("connected", () => {
+  console.log("Conectado com MongoDB.");
+});
+
+mongoose.connection.on("error", err => {
+  console.error(`Deu erro ${err}`);
 });
 
 app.listen(port, () => {
-  // tslint:disable-next-line:no-console
   console.log(`Servidor rodando na porta ${port}`);
 });
